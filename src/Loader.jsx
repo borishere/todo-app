c769b8ababd3b9f604347050c152896aad61b866
Boris <boris_here@mail.ru>
2020-10-30 20:50:08 +0300

    move loader styles to separate file

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './loader.css';
 
 function Loader() {
     return (
@@ -8,4 +9,4 @@ function Loader() {
     )
 }
 
-export default Loader
+export default Loader;
