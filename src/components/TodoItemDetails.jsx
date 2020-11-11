f183fe4bb1f2380b2ffc5baa598737da8a7f4e1f
Boris <boris_here@mail.ru>
2020-11-11 18:27:57 +0300

    added 'back' button to item details page

--- src/components/TodoItemDetails.jsx
+++ src/components/TodoItemDetails.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { Button, Container, Jumbotron } from 'react-bootstrap';
+import { useHistory, useParams } from 'react-router-dom';
 
 function TodoItemDetails({ todos }) {
     let { id } = useParams();
     let todo = todos.filter(item => item.id === +id)[0];
+    let history = useHistory();
+
+    const backBtnHandler = () => {
+        history.push('/');
+    }
 
     return (
         <Container className='text-center'>
             <Jumbotron>
                 <h2>Task id: {id}</h2>
                 <h3> {todo.title} </h3>
+                <Button
+                    variant='primary'
+                    className='mt-4'
+                    onClick={backBtnHandler}
+                >
+                    Go back
+                </Button>
             </Jumbotron>
         </Container>
     )
