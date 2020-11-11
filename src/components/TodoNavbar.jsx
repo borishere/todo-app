b3aa89327a34756d59d5f30849c7212e2d2907ba
Boris <boris_here@mail.ru>
2020-11-11 18:35:02 +0300

    click on navbar brand moves to 'home'

--- src/components/TodoNavbar.jsx
+++ src/components/TodoNavbar.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
-export const TodoNavbar = () => (
-    <Navbar bg='light' className='justify-content-between mb-5'>
-        <Navbar.Brand className='text-primary font-weight-bold'>
-            My Todo App
-        </Navbar.Brand>
-        <Nav className="mr-sm-2">
-            <NavLink exact to='/' className='nav-link text-secondary'>
-                Todo
+export const TodoNavbar = () => {
+    const history = useHistory();
+
+    const brandClickHandler = () => {
+        history.push('/');
+    }
+    return (
+        <Navbar bg='light' className='justify-content-between mb-5'>
+            <Navbar.Brand
+                className='text-primary font-weight-bold'
+                onClick={brandClickHandler}
+            >
+                My Todo App
+            </Navbar.Brand>
+            <Nav className="mr-sm-2">
+                <NavLink exact to='/' className='nav-link text-secondary'>
+                    Todo
             </NavLink>
-            <NavLink exact to='/about' className='nav-link text-secondary'>
-                About
+                <NavLink exact to='/about' className='nav-link text-secondary'>
+                    About
             </NavLink>
-        </Nav>
-    </Navbar>
-)
+            </Nav>
+        </Navbar>
+    )
+}
